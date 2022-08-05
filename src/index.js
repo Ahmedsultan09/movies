7cc1371582274645e1a1c8e460868a89d382bce2
Ahmed Sultan <as11bs22@gmail.com>
2022-08-05 14:20:30 +0200

    deploying  website

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Movies from "./Components/Movies";
 import Series from "./Components/Series";
 import Bookmarks from "./Components/Bookmarks";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/movies" element={<Movies />} />
       <Route path="/tv-series" element={<Series />} />
       <Route path="/bookmarks" element={<Bookmarks />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
